31f18d35ed76657e6b2902850090badbdc6c80aa
Gabriel de Oliveira <gabriel.oliveira00619628@sesisenaipr.org.br>
2024-04-05 14:56:59 -0300

    BackUp
    Backup Projeto Vendas - Hardware

---
+++ Arquivo_Backup/Arquivo_Backup/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Arquivo_Backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            pictureBox8 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(1, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1300, 875);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(294, 311);
+            label1.Name = "label1";
+            label1.Size = new Size(214, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Servidor HP Proliant DL360 G9 ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(547, 311);
+            label2.Name = "label2";
+            label2.Size = new Size(210, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Servidor Dell PowerEdge R720";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(793, 320);
+            label3.Name = "label3";
+            label3.Size = new Size(216, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Servidor HP Proliant ML350 G8 ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1057, 320);
+            label4.Name = "label4";
+            label4.Size = new Size(213, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Servidor Dell PowerEdge T630 ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(294, 674);
+            label5.Name = "label5";
+            label5.Size = new Size(214, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Servidor Dell PowerEdge R420 ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(547, 674);
+            label6.Name = "label6";
+            label6.Size = new Size(210, 20);
+            label6.TabIndex = 6;
+            label6.Text = "Servidor HP Proliant DL380 G7";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(793, 674);
+            label7.Name = "label7";
+            label7.Size = new Size(210, 20);
+            label7.TabIndex = 7;
+            label7.Text = "Servidor Dell PowerEdge R740";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(1057, 674);
+            label8.Name = "label8";
+            label8.Size = new Size(179, 20);
+            label8.TabIndex = 8;
+            label8.Text = "Servidor PowerEdge R450";
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(806, 467);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(188, 168);
+            pictureBox8.TabIndex = 18;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(556, 467);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(188, 168);
+            pictureBox2.TabIndex = 19;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(307, 467);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(188, 168);
+            pictureBox3.TabIndex = 20;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(307, 61);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(188, 168);
+            pictureBox7.TabIndex = 21;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(556, 61);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(188, 168);
+            pictureBox4.TabIndex = 22;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(806, 61);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(188, 168);
+            pictureBox5.TabIndex = 23;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(1057, 61);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(188, 168);
+            pictureBox6.TabIndex = 24;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Location = new Point(1057, 467);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(188, 168);
+            pictureBox9.TabIndex = 25;
+            pictureBox9.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1282, 828);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox8);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox9;
+    }
+}
---
+++ Arquivo_Backup/Arquivo_Backup/Form1.cs
@@ -0,0 +1,15 @@
+namespace Arquivo_Backup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
